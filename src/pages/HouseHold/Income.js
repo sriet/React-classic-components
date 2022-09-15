01661bcf93b2d5c9ff838e8df2460e541f2243e3
Smarttabdev <smarttabdev@gmail.com>
2022-09-15 08:13:57 -0700

    add new tab

--- src/pages/HouseHold/Income.js
+++ src/pages/HouseHold/Income.js
@@ -8,17 +8,17 @@ const WageProgressProps = [
     {
         price: 82500,
         totalPrice: 180000,
-        info:0
+        info: 0
     },
     {
         price: -35475,
         totalPrice: -57600,
-        info:1
+        info: 1
     },
     {
         price: 47025,
         totalPrice: 122400,
-        info:0
+        info: 0
     },
 ]
 
@@ -26,17 +26,17 @@ const InvestmentProgressProps = [
     {
         price: 12138,
         totalPrice: 230381,
-        info:0
+        info: 0
     },
     {
         price: 4218,
         totalPrice: 8274,
-        info:0
+        info: 0
     },
     {
         price: 938,
         totalPrice: 2103,
-        info:1
+        info: 1
     },
 ]
 
@@ -45,27 +45,27 @@ const GoogleProps1 = {
     logo: 'Google.png',
     dims: [
         {
-            title: 'Gross', 
+            title: 'Gross',
             value: 7500,
             info: 1,
             subtitle: '',
         },
         {
-            title: 'Deductions', 
+            title: 'Deductions',
             value: -3225,
             info: 1,
             subtitle: '',
         },
         {
-            title: 'Net-to-bank', 
+            title: 'Net-to-bank',
             value: 4275,
             info: 0,
             subtitle: '',
         },
     ],
     updated: 48,
-    link_1:['Add Paystub', ''],
-    link_2:['View All Paystubs', '']
+    link_1: ['Add Paystub', ''],
+    link_2: ['View All Paystubs', '']
 };
 
 const GoogleProps2 = {
@@ -73,27 +73,27 @@ const GoogleProps2 = {
     logo: 'Google.png',
     dims: [
         {
-            title: 'Long Term Gains, YTD', 
+            title: 'Long Term Gains, YTD',
             value: 12138,
             info: 0,
             subtitle: ['Unrealized', 281394],
         },
         {
-            title: 'Short Term Gains, YTD', 
+            title: 'Short Term Gains, YTD',
             value: 0,
             info: 0,
             subtitle: ['Unrealized', 18293],
         },
         {
-            title: 'Dividends, YTD', 
+            title: 'Dividends, YTD',
             value: 0,
             info: 0,
             subtitle: ['Unearned', 0],
         },
     ],
     updated: 91,
-    link_1:['Update Investments', ''],
-    link_2:['View Details', '']
+    link_1: ['Update Investments', ''],
+    link_2: ['View Details', '']
 };
 
 const GoogleProps3 = {
@@ -101,34 +101,34 @@ const GoogleProps3 = {
     logo: 'Ameritrade.png',
     dims: [
         {
-            title: 'Long Term Gains, YTD', 
+            title: 'Long Term Gains, YTD',
             value: 0,
             info: 0,
             subtitle: ['Unrealized', 28281],
         },
         {
-            title: 'Short Term Gains, YTD', 
+            title: 'Short Term Gains, YTD',
             value: 829,
             info: 0,
             subtitle: ['Unrealized', 2184],
         },
         {
-            title: 'Dividends, YTD', 
+            title: 'Dividends, YTD',
             value: 938,
             info: 0,
             subtitle: ['Unearned', 1271],
         },
     ],
     updated: 2,
-    link_1:['Update Investments', ''],
-    link_2:['View Details', '']
+    link_1: ['Update Investments', ''],
+    link_2: ['View Details', '']
 };
 
 const Income = () => {
-    
-    return(
+
+    return (
         <Grid container spacing={2} mb={15}>
-            <Grid item xs={6}>
+            <Grid item xs={12} md={6}>
                 <Grid container spacing={3}>
                     <Grid item xs={12}>
                         <Typography fontSize={24} fontWeight="700" >Wage Income</Typography>
@@ -153,12 +153,12 @@ const Income = () => {
                                 fontSize: "12px",
                                 padding: "16px",
                                 border: "1px dashed #BDBDBD",
-                                width:'100%',
-                                justifyContent:'start',
-                                color:'#828282',
+                                width: '100%',
+                                justifyContent: 'start',
+                                color: '#828282',
                                 textTransform: 'none'
                             }}>
-                            <AddIcon sx={{color:'#828282'}}/>
+                            <AddIcon sx={{ color: '#828282' }} />
                             <Typography>Add Wage Source</Typography>
                         </Button>
                     </Grid>
